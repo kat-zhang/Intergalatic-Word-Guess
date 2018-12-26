d623d224d94d1e7acfe78e080e2676e6ddc52dda
kathy zhang <kzhang193@gmail.com>
2018-12-25 23:42:59 -0600

    details

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -17,3 +17,28 @@ var winCount = 0;
 var lossCount = 0;
 var guessCount = 9;
 
+getWord();
+function getWord() {
+// display guessings remaining and wrong guesses
+    guessCount = 9;
+    $("#guesses").text(guessCount);
+    $("#wrong").text(wrongGuesses.join(""));
+
+// generate random guessing word
+    gameWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(gameWord);
+// determine how many blanks to display
+    wordLetters = gameWord.split("");
+    wordLength = wordLetters.length;
+
+    blankAnswer = [];
+    wrongGuesses = [];
+// display the appropirate amount of blanks
+    for (var i = 0; i < wordLength;  i++) {
+        blankAnswer.push("_");
+    }
+
+    console.log(blankAnswer);
+   
+
+};
